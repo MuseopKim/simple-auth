5f69329f5b9c7c925dcfe911e0fcdbd3a230c82f
ragdoll <museopkim0214@gmail.com>
2021-08-29 17:19:06 +0900

    Feat: Account 엔티티 추가
    id, password 길이(임시) 제한 조건 설정

---
+++ src/main/java/com/simpleauth/entity/Account.java
@@ -0,0 +1,29 @@
+package com.simpleauth.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Account {
+
+    @Id
+    @Column(length = 20)
+    private String id;
+
+    @Column(length = 30, nullable = false)
+    private String password;
+
+    @Builder
+    private Account(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+}
