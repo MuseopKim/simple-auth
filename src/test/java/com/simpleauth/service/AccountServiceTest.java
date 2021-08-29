eeccb3d37c3651b359fe8f92050d9c167278de58
ragdoll <museopkim0214@gmail.com>
2021-08-29 22:50:49 +0900

    Test: 테스트에 누락되어 있던 `@DisplayName` 추가

--- src/test/java/com/simpleauth/service/AccountServiceTest.java
+++ src/test/java/com/simpleauth/service/AccountServiceTest.java
@@ -5,6 +5,7 @@ import com.simpleauth.dto.request.CreateAccountRequestBuilder;
 import com.simpleauth.dto.response.AccountSummaryResponse;
 import com.simpleauth.entity.Account;
 import com.simpleauth.repository.AccountRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,15 +25,15 @@ class AccountServiceTest {
     @Mock
     private AccountRepository accountRepository;
 
+    @DisplayName("Account 생성이 완료되면 생성된 ID 값을 리턴한다")
     @Test
     void createByTest() {
         // given
-        CreateAccountRequest request = CreateAccountRequestBuilder
-                .newBuilder()
-                .id("UserID")
-                .password("password")
-                .confirmPassword("password")
-                .build();
+        CreateAccountRequest request = CreateAccountRequestBuilder.newBuilder()
+                                                            .id("UserID")
+                                                            .password("password")
+                                                            .confirmPassword("password")
+                                                            .build();
 
         Account newAccount = request.toEntity();
 
