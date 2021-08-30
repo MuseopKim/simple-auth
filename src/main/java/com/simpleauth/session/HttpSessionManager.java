326ea525526ff7a937d233455b59a9cda3aee426
ragdoll <museopkim0214@gmail.com>
2021-08-30 20:07:47 +0900

    Feat: Http 세션 관리 빈 추가
    HttpSession에 관한 전반적인 처리를 위해 추가

---
+++ src/main/java/com/simpleauth/session/HttpSessionManager.java
@@ -0,0 +1,32 @@
+package com.simpleauth.session;
+
+import com.simpleauth.dto.login.LoginAccount;
+import com.simpleauth.entity.Account;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Getter
+@Component
+public class HttpSessionManager {
+
+    private static final String LOGIN_ACCOUNT = "loginAccount";
+
+    public void login(HttpServletRequest request, Account account) {
+        HttpSession session = request.getSession(true);
+        session.setMaxInactiveInterval(60 * 30);
+        session.setAttribute(LOGIN_ACCOUNT, LoginAccount.from(account));
+    }
+
+    public boolean isLogin(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        return session == null;
+    }
+
+    public LoginAccount getLoginAccount(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        return (LoginAccount) session.getAttribute(LOGIN_ACCOUNT);
+    }
+}
